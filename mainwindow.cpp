4407a21f2dd11e3002f18aa40bb06e14d61cde74
Lizhifun <admin@lizhifun.cn>
2021-01-22 10:26:02 +0800

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,107 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QAction>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QMessageBox>
+#include <QSettings>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //由于创建菜单栏和工具栏的时候就把动作加进去，所以这里需要先创建动作Action
+    CreateActions();//创建动作Action
+    CreatMenuBar();//创建菜单栏
+    CreateToolbar();//创建工具栏
+    fileTool->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);//设置工具栏中图标显示在文字上方
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::CreateActions()//实例化下拉菜单功能
+{
+    const QIcon readActIcon =QIcon(":/imgs/read.png");
+    readAct = new QAction(readActIcon, QStringLiteral("读取文件(&O)"),this);
+    readAct->setStatusTip(QStringLiteral("读取文件"));
+    readAct->setShortcuts(QKeySequence::Open);//打开快捷键
+    connect(readAct, &QAction::triggered, this, &MainWindow::ReadFile);
+
+    const QIcon writeActIcon =QIcon(":/imgs/write.png");
+    writeAct = new QAction(writeActIcon, QStringLiteral("写入文件(&N)"), this);
+    writeAct->setStatusTip(QStringLiteral("写入文件"));
+    writeAct->setShortcuts(QKeySequence::New);//创建快捷键
+    connect(writeAct, &QAction::triggered, this, &MainWindow::WriteFile);
+
+    const QIcon updateActIcon =QIcon(":/imgs/update.png");
+    updateAct = new QAction(updateActIcon, QStringLiteral("更新文件(&U)"), this);
+    updateAct->setStatusTip(QStringLiteral("更新文件"));
+    connect(updateAct, &QAction::triggered, this, &MainWindow::UpdateFile);
+
+    const QIcon exitActIcon =QIcon(":/imgs/exit.png");
+    exitAct = new QAction(exitActIcon, QStringLiteral("退出系统(&E)"), this);
+    exitAct->setStatusTip(QStringLiteral("退出系统"));
+    exitAct->setShortcuts(QKeySequence::Close);//退出系统快捷键
+    connect(exitAct, &QAction::triggered, this, &MainWindow::ExitProgram);
+}
+
+void MainWindow::CreatMenuBar()
+{
+    fileMenu = menuBar()->addMenu(QStringLiteral("文件").trimmed()+tr("(&F)"));//实例化一个菜单栏
+    operaMenu = menuBar()->addMenu(QStringLiteral("操作").trimmed()+tr("(&A)"));//实例化一个菜单栏
+    fileMenu->addAction(readAct);
+    fileMenu->addAction(writeAct);
+    fileMenu->addAction(updateAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAct);
+}
+
+void MainWindow::CreateToolbar()//实例化工具栏
+{
+    fileTool = addToolBar(QStringLiteral("文件"));
+    fileTool->addAction(readAct);
+    fileTool->addAction(writeAct);
+    fileTool->addAction(updateAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAct);
+}
+
+void MainWindow::ReadFile()
+{
+    QSettings *settings = new QSettings("./file.ini",QSettings::IniFormat);//调用QSettings操作ini文件
+    QString lang = settings->value("content","").toString();//读出ini文件中保存的内容，默认为空
+    ui->textEdit->setText(lang);//将读出的内容显示到窗口中
+    delete settings;
+    QMessageBox::information(this,QStringLiteral("提示"), QStringLiteral("读取成功!"));
+}
+
+void MainWindow::WriteFile()
+{
+    QSettings *settings = new QSettings("./file.ini",QSettings::IniFormat);
+    settings->setValue("content","hello, this is a file!");//将内容写入ini文件中
+    delete settings;
+    QMessageBox::information(this,QStringLiteral("提示"), QStringLiteral("写入成功!"));
+}
+
+void MainWindow::UpdateFile()
+{
+    QSettings *settings = new QSettings("./file.ini",QSettings::IniFormat);
+    settings->setValue("content",ui->textEdit->toPlainText());//将当前选择的语言写入ini文件中
+    delete settings;
+    QMessageBox::information(this,QStringLiteral("提示"), QStringLiteral("更新成功!"));
+}
+
+void MainWindow::ExitProgram()
+{
+    QMessageBox::StandardButton btn;
+    btn = QMessageBox::question(this, QStringLiteral("提示"), QStringLiteral("是否退出系统?"), QMessageBox::Yes|QMessageBox::No);
+    if (btn == QMessageBox::Yes) {//如果选择是
+        this->close();//退出系统
+    }
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,40 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void CreateActions();//创建下拉菜单函数
+    void CreatMenuBar();//创建菜单栏函数
+    void CreateToolbar();//创建工具栏函数
+
+    void ReadFile();//读取文件函数
+    void WriteFile();//写文件函数
+    void UpdateFile();//更新文件函数
+    void ExitProgram();//退出程序
+
+private:
+    Ui::MainWindow *ui;
+
+    QMenu *fileMenu;//声明菜单栏
+    QMenu *operaMenu;//声明菜单栏
+
+    QToolBar *fileTool;//声明工具栏
+
+    QAction *readAct;//声明读文件菜单
+    QAction *writeAct;//声明写文件菜单
+    QAction *updateAct;//声明更新文件菜单
+    QAction *exitAct;//声明推出程序菜单
+};
+#endif // MAINWINDOW_H
